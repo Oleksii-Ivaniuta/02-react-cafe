69a367bdb3640e32d3a883d53c810b4f0ecd5e35
Oleksii Ivaniuta <11.ivaniuta@gmail.com>
2025-05-10 14:19:48 +0100

    add files

---
+++ src/components/app/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  console.log(`add files`);
+  
+  return (
+    <div>
+      <p>Add Files</p>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
-// src/main.tsx
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
-// Нормалізація стилів
+import App from "./components/app/App";
 import "modern-normalize";
-// Глобальні стилі (додатково)
 import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
