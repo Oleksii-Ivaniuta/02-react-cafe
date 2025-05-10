bbbc26e60d124704e2a0086cb14813134b42a1f3
Oleksii Ivaniuta <11.ivaniuta@gmail.com>
2025-05-10 17:29:38 +0100

    add destruct

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,11 +1,10 @@
-import type { Votes } from "../../types/votes";
-import CafeInfo from "../cafeinfo/CafeInfo";
-import VoteOptions from "../voteoptions/VoteOptions";
-import VoteStats from "../votestats/VoteStats";
-import Notification from "../notification/Notification";
-import css from "./App.module.css";
-import { useState } from "react";
-
+import type { Votes } from '../../types/votes';
+import CafeInfo from '../cafeinfo/CafeInfo';
+import VoteOptions from '../voteoptions/VoteOptions';
+import VoteStats from '../votestats/VoteStats';
+import Notification from '../notification/Notification';
+import css from './App.module.css';
+import { useState } from 'react';
 
 export default function App() {
   const [votes, setVotes] = useState<Votes>({ good: 0, bad: 0, neutral: 0 });
@@ -21,13 +20,29 @@ export default function App() {
       good: 0,
       bad: 0,
       neutral: 0,
-    })
-  }
+    });
+  };
   const totalVotes: number = votes.good + votes.bad + votes.neutral;
-  const positiveRate: number = totalVotes ? Math.round((votes.good / totalVotes) * 100) : 0;
-  return <div className={css.app}>
-    <CafeInfo />
-    <VoteOptions onVote = {handleVote} onReset={resetVotes} canReset={totalVotes ? true : false}/>
-    {totalVotes ?  <VoteStats votes={votes} totalVotes={totalVotes} positiveRate={positiveRate} /> : <Notification />}
-  </div>;
+  const positiveRate: number = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes ? true : false}
+      />
+      {totalVotes ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
 }
